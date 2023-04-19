51418a38af39311a70339e3123070339a7895639
zlatka-n <zla.b.ngu@gmail.com>
2023-04-19 11:36:48 +0200

    remove if (reservation)

--- index.js
+++ index.js
@@ -17,30 +17,28 @@ async function run() {
   if (buttonCookies) await buttonCookies.click();
 
   ///Navigate to login page
-  if (reservation) {
 
-    await Promise.all([
-      reservation.click(),
-      page.waitForNavigation
-    ])
+  await Promise.all([
+    reservation.click(),
+    page.waitForNavigation
+  ])
 
-    //Accept all cookies again
-    await page.waitForSelector(".CybotCookiebotDialogBodyButton")
-    await page.click("button[id='CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll']")
+  //Accept all cookies again
+  await page.waitForSelector(".CybotCookiebotDialogBodyButton")
+  await page.click("button[id='CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll']")
 
-    //Login
-    const cardNumber = await page.$("input[name='uid']")
-    const password = await page.$("input[name='heslo']")
+  //Login
+  const cardNumber = await page.$("input[name='uid']")
+  const password = await page.$("input[name='heslo']")
 
-    await cardNumber.type(CARD_NUMBER)
-    await password.type(PASSWORD)
+  await cardNumber.type(CARD_NUMBER)
+  await password.type(PASSWORD)
 
-    const logInBtn = await page.$("button[class='button log-in g-recaptcha']")
+  const logInBtn = await page.$("button[class='button log-in g-recaptcha']")
 
-    logInBtn.click()
-  }
+  logInBtn.click()
 
-  //await browser.close()
+  await browser.close()
 
 }
 
